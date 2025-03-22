423e0a2357fef43936a76bf87263129310096798
vibgyorvip <vibgyorvipul786@gmail.com>
2025-03-22 18:58:18 +0530

    Spring boot :  Dependency Injection : Setter Injection

--- src/main/java/com/programmingpointer/SpringBootComplete/Entity/primeUser.java
+++ src/main/java/com/programmingpointer/SpringBootComplete/Entity/primeUser.java
@@ -6,12 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class primeUser {
 
-    @Autowired
+    //@Autowired
     Order order;
 
+    @Autowired
+    public void setOrderDependency(Order order){
+        this.order = order;
+    }
     public primeUser(){
         System.out.println("initializing primeUser");
     }
+
 
 
 }
