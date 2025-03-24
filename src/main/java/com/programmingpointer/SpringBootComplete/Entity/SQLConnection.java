460d005e0c34c588b50e15fdcebb592b0f1b7109
vibgyorvip <vibgyorvipul786@gmail.com>
2025-03-24 18:10:47 +0530

    Sprigboot: @Profile

--- src/main/java/com/programmingpointer/SpringBootComplete/Entity/SQLConnection.java
+++ src/main/java/com/programmingpointer/SpringBootComplete/Entity/SQLConnection.java
@@ -2,9 +2,11 @@ package com.programmingpointer.SpringBootComplete.Entity;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("prod")
 public class SQLConnection {
 
     @Value("${username}")
